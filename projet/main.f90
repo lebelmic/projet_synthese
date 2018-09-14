939c70715d18d0489a09710919b044f5fdc6022f
Gabriel Antonius <gabriel.antonius@gmail.com>
2018-09-14 09:46:12 -0400

    Change to f90 fix mpi

--- projet/main.f90
+++ projet/main.f90
@@ -8,9 +8,9 @@ call MPI_INIT(ierr)
 
 call MPI_COMM_RANK(MPI_COMM_WORLD, procid, ierr)
 call MPI_COMM_SIZE(MPI_COMM_WORLD, numprocs, ierr)
-    print *, "Hello World! I am process ", procid, " out of ", numprocs, "!
+write (*,*) "Hello World! I am process ", procid, " out of ", numprocs, "!"
 
-call MPI_FINALIZE
+call MPI_FINALIZE(ierr)
 
 stop
 end
