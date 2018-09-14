1f9e374ffa768e07740c19dcb15036218eb0e03b
Gabriel Antonius <gabriel.antonius@gmail.com>
2018-09-14 10:17:16 -0400

    Changed the printed message.

--- projet/main.f90
+++ projet/main.f90
@@ -9,7 +9,7 @@ call MPI_INIT(ierr)
 call MPI_COMM_RANK(MPI_COMM_WORLD, procid, ierr)
 call MPI_COMM_SIZE(MPI_COMM_WORLD, numprocs, ierr)
 
-write (*,*) "Hello World! I am process ", procid, " out of ", numprocs, "!"
+write (*,*) "Hello World! I am process ", procid, " out of ", numprocs
 
 call MPI_FINALIZE(ierr)
 
