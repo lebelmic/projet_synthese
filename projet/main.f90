bf8871d092d809dcd7fdfbec1ea2414db5430f44
Gabriel Antonius <gabriel.antonius@gmail.com>
2018-09-14 10:00:30 -0400

    Cosmetic changes

--- projet/main.f90
+++ projet/main.f90
@@ -2,15 +2,15 @@
 ! By WQY
 program main
 use mpi
-integer ierr, numprocs, procid
+integer :: ierr, numprocs, procid
 
 call MPI_INIT(ierr)
 
 call MPI_COMM_RANK(MPI_COMM_WORLD, procid, ierr)
 call MPI_COMM_SIZE(MPI_COMM_WORLD, numprocs, ierr)
+
 write (*,*) "Hello World! I am process ", procid, " out of ", numprocs, "!"
 
 call MPI_FINALIZE(ierr)
 
-stop
-end
+end program main
